442ee20b7b2288dda3b0dbae5fe7d071e1c58026
许 <m6127784376@163.com>
2026-05-24 00:01:37 +0800

    给VO添加Swagger Schema注解

--- src/main/java/com/liushipin/userorderauthsystem/vo/LoginVO.java
+++ src/main/java/com/liushipin/userorderauthsystem/vo/LoginVO.java
@@ -1,25 +1,21 @@
 package com.liushipin.userorderauthsystem.vo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 /**
  * 登录成功后返回给前端的数据
  */
 @Data
+@Schema(description = "登录成功后返回给前端的数据")
 public class LoginVO {
 
-    /**
-     * 用户 ID
-     */
+    @Schema(description = "用户 ID", example = "123")
     private Long userId;
 
-    /**
-     * 用户名
-     */
+    @Schema(description = "用户名", example = "zhangsan")
     private String username;
 
-    /**
-     * JWT token
-     */
+    @Schema(description = "JWT 令牌", example = "")
     private String token;
 }
--- src/main/java/com/liushipin/userorderauthsystem/vo/UserVO.java
+++ src/main/java/com/liushipin/userorderauthsystem/vo/UserVO.java
@@ -1,5 +1,6 @@
 package com.liushipin.userorderauthsystem.vo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -11,31 +12,22 @@ import java.time.LocalDateTime;
  * 1. 控制接口返回给前端的数据
  * 2. 避免把 password 等敏感字段返回出去
  */
+@Schema(description = "用户返回对象")
 @Data
 public class UserVO {
 
-    /**
-     * 用户 ID
-     */
+    @Schema(description = "用户 ID", example = "1")
     private Long id;
 
-    /**
-     * 用户名
-     */
+    @Schema(description = "用户名", example = "zhangsan")
     private String username;
 
-    /**
-     * 昵称
-     */
+    @Schema(description = "用户昵称", example = "张三")
     private String nickname;
 
-    /**
-     * 创建时间
-     */
+    @Schema(description = "创建时间",example = "2024-06-01T12:00:00")
     private LocalDateTime createTime;
 
-    /**
-     * 更新时间
-     */
+    @Schema(description = "更新时间", example = "2024-06-01T12:00:00")
     private LocalDateTime updateTime;
 }
